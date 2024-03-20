eac372a7f180e8b98b7d34f868de98800cf18ba4
GustavoBento <gustavo_grb-@hotmail.com>
2024-03-20 19:50:36 -0300

    Criando a ordenação

---
+++ src/components/Ordenacao/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export default function Ordenacao() {
+    return (
+        <section className='container-ordenacao'>
+            <p className='ordenacao-destaque'>Recentes</p>
+            <p className='ordenacao-cinza'>Recentes</p>
+        </section>
+    )
+}
